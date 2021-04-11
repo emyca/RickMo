1d0bf762bb111cfa9aac4a8e665334bc40db74d4
emyca <evmyca@gmail.com>
2021-04-11 11:32:49 +0300

    Add splash screen

--- app/src/main/java/ua/kh/em/rickmo/ui/view/MainActivity.kt
+++ app/src/main/java/ua/kh/em/rickmo/ui/view/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
